e933e8fcd7a1745a5b7c4475e42dfeaeedc879a6
Jean Paulo Talar <jeanttalar@gmail.com>
2023-02-27 16:46:04 -0300

    Primeiro commit até a segunda aula finalizada

---
+++ ByteBank.View/MainWindow.xaml.cs
@@ -0,0 +1,79 @@
+﻿using ByteBank.Core.Model;
+using ByteBank.Core.Repository;
+using ByteBank.Core.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ByteBank.View
+{
+    public partial class MainWindow : Window
+    {
+        private readonly ContaClienteRepository r_Repositorio;
+        private readonly ContaClienteService r_Servico;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            r_Repositorio = new ContaClienteRepository();
+            r_Servico = new ContaClienteService();
+        }
+
+        private void BtnProcessar_Click(object sender, RoutedEventArgs e)
+        {
+            var taskSchedulerUI = TaskScheduler.FromCurrentSynchronizationContext();
+
+            BtnProcessar.IsEnabled = false;
+
+            var contas = r_Repositorio.GetContaClientes();
+
+            var resultado = new List<string>();
+
+            AtualizarView(new List<string>(), TimeSpan.Zero);
+
+            var inicio = DateTime.Now;
+
+            var contasTarefas = contas.Select(conta =>
+            {
+                return Task.Factory.StartNew(() =>
+                {
+                    var resultadoConta = r_Servico.ConsolidarMovimentacao(conta);
+                    resultado.Add(resultadoConta);
+                });
+            }).ToArray();
+
+            Task.WhenAll(contasTarefas)
+                .ContinueWith(task =>
+                {
+                    var fim = DateTime.Now;
+                    AtualizarView(resultado, fim - inicio);
+                }, taskSchedulerUI)
+                .ContinueWith(task =>
+                {
+                    BtnProcessar.IsEnabled = true;
+                }, taskSchedulerUI);
+        }
+
+        private void AtualizarView(List<String> result, TimeSpan elapsedTime)
+        {
+            var tempoDecorrido = $"{ elapsedTime.Seconds }.{ elapsedTime.Milliseconds} segundos!";
+            var mensagem = $"Processamento de {result.Count} clientes em {tempoDecorrido}";
+
+            LstResultados.ItemsSource = result;
+            TxtTempo.Text = mensagem;
+        }
+    }
+}
---
+++ TestesConsole/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TestesConsole
+{
+    class Program
+    {
+        public static long FatorialEmParalelo(int valor)
+        {
+            long resultado = 0;
+
+            var t_calculo = new Thread(() => resultado = Fatorial(valor));
+            t_calculo.Start();
+
+            while (t_calculo.IsAlive)
+            {
+
+            }
+
+            return resultado;
+        }
+
+        private static long Fatorial(int valor)
+        {
+            var resultado = 1L;
+            for (int i = valor; i >= 2; i--)
+                resultado *= i;
+            return resultado;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(FatorialEmParalelo(5));
+        }
+    }
+}
